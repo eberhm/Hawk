4ffd49dd722d16ee8f72eb8e539611bb3259b9f3
Christian Soronellas <theunic@gmail.com>
2011-09-19 07:33:58 +0200

    Added a unit test suite for the Server class.

---
+++ test/Hawk/Http/ServerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the Hawk Http Server
+ *
+ * (c) Christian Soronellas <christian@sistemes-cayman.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HawkTest\Http;
+
+/**
+ * A test suite for the Server class
+ *
+ * @author Christian Soronellas <christian@sistemes-cayman.es>
+ */
+class ServerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * The server instance
+     * 
+     * @var \Hawk\Http\Server
+     */
+    protected $_server;
+    
+    public function setUp()
+    {
+        $this->_server = new \Hawk\Http\Server();
+    }
+    
+    public function tearDown()
+    {
+        $this->_server = null;
+    }
+    
+    public function testPortCannotBeAString()
+    {
+        $this->_server->setPort('abc');
+        
+        $this->assertEquals(9000, $this->_server->getPort());
+    }
+    
+    public function testAddressCanOnlyBeIpv4OrIpv6()
+    {
+        $this->_server->setAddress('abc');
+        
+        $this->assertEquals('0.0.0.0', $this->_server->getAddress());
+    }
+}
